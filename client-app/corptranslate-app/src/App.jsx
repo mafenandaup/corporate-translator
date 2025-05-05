562effbc147996057c9b45fa2201fc26dabd53bf
Maria Fernanda <mafenandaup@gmail.com>
2025-05-05 18:20:23 -0300

    build: install fontawesome

--- client-app/corptranslate-app/src/App.jsx
+++ client-app/corptranslate-app/src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import corpTransIcon from './../public/assets/CORPTRANSLATOR LOGO.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRepeat } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
@@ -11,6 +13,9 @@ function App() {
         <div className="introduction">
           <img src={corpTransIcon} alt="logo - Corporate Translator" className='myLogo' />
           <h3>Saiba o que você está falando(ou ouvindo)</h3>
+          <input type="text" name="translate-phrase" id="" placeholder='Sua frase aqui' />
+          <button><img src={faRepeat} alt="switch" /></button>
+          <input type="submit" value="Traduzir" />
         </div>
       </section>
     </>
