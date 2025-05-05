ffabd65a6c96c8948553acd192be9e1cd4b41be7
Maria Fernanda <mafenandaup@gmail.com>
2025-05-05 18:39:23 -0300

    refactor: alter html

--- client-app/corptranslate-app/src/App.jsx
+++ client-app/corptranslate-app/src/App.jsx
@@ -13,8 +13,11 @@ function App() {
         <div className="introduction">
           <img src={corpTransIcon} alt="logo - Corporate Translator" className='myLogo' />
           <h3>Saiba o que você está falando(ou ouvindo)</h3>
-          <input type="text" name="translate-phrase" id="" placeholder='Sua frase aqui' />
-          <button><img src={faRepeat} alt="switch" /></button>
+          <div className="textbox-contain">
+          <input type="text" name="translate-phrase" id="" className='textbox' placeholder='Sua frase aqui' />
+            <button><FontAwesomeIcon icon={faRepeat}></FontAwesomeIcon></button>
+            <textarea name="" id="" className='textbox'></textarea>
+          </div>
           <input type="submit" value="Traduzir" />
         </div>
       </section>
