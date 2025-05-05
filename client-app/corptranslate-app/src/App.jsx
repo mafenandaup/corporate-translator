32577823bde32a51741a65e94a1f71b78464f0e4
Maria Fernanda <mafenandaup@gmail.com>
2025-05-05 18:45:28 -0300

    refactor: add hover animation

--- client-app/corptranslate-app/src/App.jsx
+++ client-app/corptranslate-app/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import corpTransIcon from './../public/assets/CORPTRANSLATOR LOGO.png'
+import { motion } from "motion/react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRepeat } from '@fortawesome/free-solid-svg-icons';
 
@@ -15,10 +16,10 @@ function App() {
           <h3>Saiba o que você está falando(ou ouvindo)</h3>
           <div className="textbox-contain">
           <input type="text" name="translate-phrase" id="" className='textbox' placeholder='Sua frase aqui' />
-            <button><FontAwesomeIcon icon={faRepeat}></FontAwesomeIcon></button>
+            <motion.button whileHover={{scale: 1.1}} className='action-btns'><FontAwesomeIcon icon={faRepeat}></FontAwesomeIcon></motion.button>
             <textarea name="" id="" className='textbox'></textarea>
           </div>
-          <input type="submit" value="Traduzir" />
+          <motion.input whileHover={{scale: 1.1}}type="submit" value="Traduzir" className='action-btns'/>
         </div>
       </section>
     </>
