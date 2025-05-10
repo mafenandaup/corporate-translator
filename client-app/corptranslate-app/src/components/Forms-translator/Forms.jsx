48b87d4f0177c3c686226b196f158d562b423398
Maria Fernanda <mafenandaup@gmail.com>
2025-05-10 10:59:42 -0300

    feat: clear inputs btn

--- client-app/corptranslate-app/src/components/Forms-translator/Forms.jsx
+++ client-app/corptranslate-app/src/components/Forms-translator/Forms.jsx
@@ -3,6 +3,7 @@ import InputBtns from '../Input-btns/InputBtns';
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRepeat } from '@fortawesome/free-solid-svg-icons';
 // import React, { useState } from 'react'; comment 4 now
 
 function TranslateForms() {
@@ -13,9 +14,10 @@ function TranslateForms() {
             <div className="textbox-contain">
                 <label htmlFor="translation-phrase">Linguagem comum</label>
                 <textarea type="text" name="translation-phrase" id="translation-phrase" className='textbox' />
+                <motion.button whileHover={{ scale: 1.1 }} className='action-btns' id='switch-context'>Trocar contexto <FontAwesomeIcon icon={faRepeat}></FontAwesomeIcon></motion.button>
                 <label htmlFor="translated-phrase">Linguagem corporativa</label>
                 <textarea name="translated-phrase" id="translated-phrase" className='textbox' disabled='disabled'></textarea>
-                <InputBtns/>
+                <InputBtns />
             </div>
         </>
     )
--- client-app/corptranslate-app/src/components/Input-btns/InputBtns.jsx
+++ client-app/corptranslate-app/src/components/Input-btns/InputBtns.jsx
@@ -4,7 +4,7 @@ import './InputBtns.css'
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRepeat } from '@fortawesome/free-solid-svg-icons';
+import { faBroom } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -13,10 +13,11 @@ function InputBtns() {
 
     return (
         <>
-               <div className="button-cont">
-                    <motion.button whileHover={{ scale: 1.1 }} className='action-btns'><FontAwesomeIcon icon={faRepeat}></FontAwesomeIcon> trocar contexto</motion.button>
-                    <motion.input whileHover={{ scale: 1.1 }} type="submit" value="Traduzir" className='action-btns' id='translate-btn' />
-                </div>
+            <div className="button-cont">
+                <motion.input whileHover={{ scale: 1.1 }} type="submit" value="Traduzir" className='action-btns' id='translate-btn' />
+                <motion.button whileHover={{ scale: 1.1 }} className='action-btns' id='clear-text-btn'>Limpar Texto <FontAwesomeIcon icon={faBroom}></FontAwesomeIcon></motion.button>
+
+            </div>
         </>
     )
 }
