b4a57d4451f4276c16066ebd8cbdc61b44e88267
Maria Fernanda <mafenandaup@gmail.com>
2025-05-10 14:09:53 -0300

    feat: clear text button

--- client-app/corptranslate-app/src/components/Forms-translator/Forms.jsx
+++ client-app/corptranslate-app/src/components/Forms-translator/Forms.jsx
@@ -16,8 +16,9 @@ function TranslateForms() {
     return (
         <>
             <div className="textbox-contain">
-                <label htmlFor="translation-phrase">Linguagem comum</label>
-                <textarea type="text" name="translation-phrase" id="translation-phrase" value={InputTranslation} className='textbox'/>
+                <label htmlFor="translation-phrase">Linguagem comum</label> 
+                {/* o event.target é a referência ao campo que gerou o evento; nesse caso uma text área. event.target.value é o VALOR ATUAL do campo que está sendo acionado*/}
+                <textarea type="text" name="translation-phrase" id="translation-phrase" value={InputTranslation} onChange={(event) => setInputTranslation(event.target.value)} className='textbox' />
                 <motion.button whileHover={{ scale: 1.1 }} className='action-btns' id='switch-context'>Trocar contexto <FontAwesomeIcon icon={faRepeat}></FontAwesomeIcon></motion.button>
                 <label htmlFor="translated-phrase">Linguagem corporativa</label>
                 <textarea name="translated-phrase" id="translated-phrase" className='textbox' value={InputTranslated} disabled='disabled'></textarea>
--- client-app/corptranslate-app/src/components/Input-btns/InputBtns.jsx
+++ client-app/corptranslate-app/src/components/Input-btns/InputBtns.jsx
@@ -5,13 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBroom } from '@fortawesome/free-solid-svg-icons';
 
 
-function InputBtns({}) {
+function InputBtns({ clearText}) {
 
     return (
         <>
             <div className="button-cont">
                 <motion.input whileHover={{ scale: 1.1 }} type="submit" value="Traduzir" className='action-btns' id='translate-btn' />
-                <motion.button whileHover={{ scale: 1.1 }} className='action-btns' id='clear-text-btn'>Limpar Texto <FontAwesomeIcon icon={faBroom}></FontAwesomeIcon></motion.button>
+                <motion.button whileHover={{ scale: 1.1 }} className='action-btns' id='clear-text-btn' onClick={clearText}>Limpar Texto <FontAwesomeIcon icon={faBroom}></FontAwesomeIcon></motion.button>
 
             </div>
         </>
