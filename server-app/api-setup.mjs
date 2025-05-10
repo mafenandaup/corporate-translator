8323318331065bafb3df7fba70856534e2f23aeb
Maria Fernanda <mafenandaup@gmail.com>
2025-05-10 15:14:37 -0300

    refactor: add alternate prompts

--- server-app/api-setup.mjs
+++ server-app/api-setup.mjs
@@ -1,22 +1,21 @@
 import { config } from "dotenv";
 import { GoogleGenAI } from "@google/genai";
 
-// Carregando o .env
 config();
 
 const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
- const phrase = "eu já disse isso antes, será que você não entendeu?";
-    
-async function main() {
+const phrase = "eu já disse isso antes, será que você não entendeu?";
+
+async function main(context) {
+  const prompt = context === "corporative"
+    ? "Você é um tradutor especialista em adaptar frases do cotidiano para o contexto corporativo. Para cada frase que você recebe, escreva como seria dita em um email formal ou reunião, porém com um toque levemente ácido, mas ainda assim educado em sua fala. EXEMPLO: Entrada:  seu idiota, eu já disse isso antes // Saída: Caso não tenha se atentado, conforme mencionado no email anterior, ..."
+    : "Você é um tradutor especialista em simplificar linguagem técnica, transformando frases formais em algo que seria dito no cotidiano. Para cada frase que você recebe, escreva como seria dito em uma conversa informal com um amigo íntimo, com um toque levemente ácido e bem sincero, sem dar atenção à semântica. Exemplo: Entrada"
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: `Você é um tradutor especialista em adaptar frases do cotidiano para o contexto 
-    corporativo. Para cada frase que você recebe, reescreva-a como seria dita em um email formal
-    ou reunião, porém com um toque levemente ácido, mas ainda assim educado em sua fala. EXEMPLO: Entrada: " seu idiota, eu já disse isso antes"
-    Saída: "Caso não tenha se atentado, conforme mencionado no email anterior, ..."
-    Por favor, traduza a frase: "${phrase}"`,
+    contents: `${prompt} Por favor, traduza a frase: "${phrase}"`,
   });
+
   console.log(response.text);
 }
 
